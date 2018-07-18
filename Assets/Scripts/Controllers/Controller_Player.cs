c6f5fee9abafc59363926c6ac62636b39d46a1ff
Christopher Koon <chrkoon@uat.edu>
2018-07-18 00:04:40 -0700

    Art Added

---
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : Controller {
+
+	// Use this for initialization
+	protected override void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	protected override void Update ()
+    {
+        inputHandler();
+	}
+
+    void inputHandler()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Move left
+            // Flip left
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Move right
+            // flip right
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Climb
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Crouch
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Jump
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Open in game menu
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            // Dash
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            // Interact
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Controller_Player player;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
